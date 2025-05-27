4f208e88b51d6a33d298cd6ad896ddbe087f8249
Dajan Medina <59675922+dmedina2150dev@users.noreply.github.com>
2025-05-27 16:28:22 -0400

    Add variables enteras

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 
 	hello "github.com/dmedina2150dev/learn-go/hola-mundo"
+	"main/variables"
 )
 
 func main() {
 	fmt.Println("Hola Mundo!!!")
 	hello.SayHello()
+	variables.MuestroEnteros()
 }
