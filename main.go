bfeea276f4ccb5806ee1003bbe4521b82c23ede4
Dajan Medina <dmedina2150dev@gmail.com>
2025-05-27 21:33:49 -0400

    eliminar llamadas a funciones y comentarios en main.go

--- main.go
+++ main.go
@@ -3,24 +3,24 @@ package main
 import (
 	"fmt"
 
-	controldeflujos "github.com/dmedina2150dev/learn-go/control-de-flujos"
+	// controldeflujos "github.com/dmedina2150dev/learn-go/control-de-flujos"
 	// "github.com/dmedina2150dev/learn-go/ejercicios"
 	hello "github.com/dmedina2150dev/learn-go/hola-mundo"
-	"github.com/dmedina2150dev/learn-go/variables"
+	// "github.com/dmedina2150dev/learn-go/variables"
 )
 
 func main() {
-	fmt.Println("Hola Mundo!!!")
+	fmt.Println("Iniciando GO!")
 	hello.SayHello()
-	variables.MuestroEnteros()
-	variables.RestoVariables()
+	// variables.MuestroEnteros()
+	// variables.RestoVariables()
 
-	state, text := variables.ConvertToText(1950)
+	// state, text := variables.ConvertToText(1950)
 
-	fmt.Println(state)
-	fmt.Println(text)
+	// fmt.Println(state)
+	// fmt.Println(text)
 
-	fmt.Print("\n\n")
+	// fmt.Print("\n\n")
 
 	// var dato string
 	// fmt.Print("Ingrea un número entero:")
@@ -30,5 +30,5 @@ func main() {
 
 	// fmt.Printf("valor: %d, y %s \n", value, msg)
 
-	controldeflujos.Iterar()
+	// controldeflujos.Iterar()
 }
